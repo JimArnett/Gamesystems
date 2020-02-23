3708b9f9b8835af1c327559c26165d70461ea799
JimArnett <jimarnett98@gmail.com>
2020-02-23 17:32:01 -0600

    Updated script and added anims

--- Mr_Electronic/Assets/Scripts/player.cs
+++ Mr_Electronic/Assets/Scripts/player.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class player : MonoBehaviour
 {
     public float thrust = 12f;
     public float timeLeft = 4f;
     public Slider electricity;
+    public GameObject slider;
 
     // Start is called before the first frame update
     void Start()
@@ -25,32 +27,54 @@ public class player : MonoBehaviour
     private void OnTriggerStay(Collider other) {
         if (other.tag == "insulator"){
             timeLeft -= Time.deltaTime;
-                if (timeLeft < .5f){
+                if (timeLeft < 0f){
                     timeLeft = 4f;
-                    if (thrust <= 2f){
-                        thrust = 2f;
+                    if (thrust == 4f){
+                        thrust -= 2f;
                         electricity.value = thrust;
+                        //possible animation letting player know they only have one tick left
+                        slider.GetComponent<Animator>().SetTrigger("danger");
+                        Debug.Log("fuck");
                     }
                     else{
-                        thrust -= 2;
+                        thrust -= 2f;
                         electricity.value = thrust;
+                    }
+                    if (thrust <= 0f){
+                        SceneManager.LoadScene(0);
                     }
                 }
         }
 
         if (other.tag == "conductor"){
             timeLeft -= Time.deltaTime;
-                if (timeLeft < 0){
+                if (timeLeft < 0f){
                     timeLeft = 4f;
-                    if (thrust >= 22){
-                        thrust = 22;
+                    if (thrust == 20f){
+                        thrust += 2f;
                         electricity.value = thrust;
+                        //possible animation letting player know they only have one tick left
+                        slider.GetComponent<Animator>().SetTrigger("danger");
                     }
                     else{
-                        thrust += 2;
+                        thrust += 2f;
                         electricity.value = thrust;
+                    }
+                    if (thrust >= 24f){
+                        SceneManager.LoadScene(0);
                     }
                 }
         }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.tag == "insulator"){
+            timeLeft = 4f;
+        }
+
+        if (other.tag == "conductor"){
+            timeLeft = 4f;
+        }
+
     }
 }
